34dba6fae41dd392f2f0ccee1cb1fca4dec35994
Orizonaru <146365147+Orizonaru@users.noreply.github.com>
2024-12-09 23:28:16 +0300

    class init

--- app/default_requests_processing.py
+++ app/default_requests_processing.py
@@ -4,3 +4,8 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
+class DefaultRequestsProcessing:
+    def __init__(self, api_url, request_timeout=30):
+        self.api_url = api_url
+        self.http_session = requests.Session()
+        self.request_timeout = request_timeout
